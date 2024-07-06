b7d258cbc2a09bdfe5768a25ccc513c8f530d353
Jevaughn Vickers <114950498+jevaughnvick@users.noreply.github.com>
2024-07-06 09:14:52 -0400

    refactor code to populate grid into a function

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const gridContainer = document.querySelector("#grid-container");
 
-let gridLength = 9;
-
-const numberOfDivs = gridLength ** 2;
-
-let flexBasis = 100 / gridLength;
-
-for(let i = 0; i < numberOfDivs; i++){
 
 
 
-    let div = document.createElement("div");
-    div.setAttribute("class", "js-made");
-    div.style.cssText = `flex-basis: ${flexBasis}%;`;
-    gridContainer.appendChild(div);
+function createGrid(gridLength){
+
+    let flexBasis = 100 / gridLength;
+
+    for(let i = 0; i < gridLength ** 2; i++){
+
+        let div = document.createElement("div");
+        div.setAttribute("class", "js-made");
+        div.style.cssText = `flex-basis: ${flexBasis}%;`;
+        gridContainer.appendChild(div);
+    }
 }
+
+
+createGrid(16)
